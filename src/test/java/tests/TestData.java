a3e98763c90d718d4456fc0820c42d501edfb00e
Anna Petrova <mail.anna.p@yandex.ru>
2022-12-08 14:03:18 +0500

    добавила еще переменных

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -8,30 +8,36 @@ import java.util.Locale;
 
 
 public class TestData {
-   static Faker faker = new Faker();
-   static SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH);
-   static String[] birthday = dateFormat.format(faker.date().birthday()).split(" ");
+    static Faker faker = new Faker();
+    static SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH);
+    static String[] birthday = dateFormat.format(faker.date().birthday()).split(" ");
 
     static String userName = faker.name().firstName(),
-           lastName = faker.name().lastName(),
-           userBirthDay = birthday[0],
-           userBirthMonth = birthday[1],
-           userBirthYear = birthday[2],
-           userAddress = faker.address().fullAddress(),
+            lastName = faker.name().lastName(),
+            userBirthDay = birthday[0],
+            userBirthMonth = birthday[1],
+            userBirthYear = birthday[2],
+            userAddress = faker.address().fullAddress(),
             userPhone = faker.phoneNumber().subscriberNumber(10);
 
-    public static String randomString(int len){
+    public static String randomString(int len) {
         String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         SecureRandom rnd = new SecureRandom();
         StringBuilder sb = new StringBuilder(len);
-        for(int i = 0; i < len; i++)
+        for (int i = 0; i < len; i++)
             sb.append(AB.charAt(rnd.nextInt(AB.length())));
         return sb.toString();
     }
 
-    public static String randomEmail(int len){
+    public static String randomEmail(int len) {
         return randomString(len) + "@guru.qa";
     }
 
-    static String userEmail = randomEmail(10);
+    static String userEmail = randomEmail(10),
+            userSex = "Other",
+            userSubject = "Maths",
+            userHobbies = "Sports",
+            userState = "NCR",
+            userCity = "Delhi";
+
 }
